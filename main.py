d3c3ad3ecc5e7b20c9cbad5e1fe5a2c31a649735
Jack Woolterton <jwoolter@gmail.com>
2020-06-08 07:07:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,249 @@
+#import the modules
+
+from colorama import *
+import random
+
+#global variables start
+
+#dictionary of npcs Key = name values = greeting, I am statement
+
+npcs = {"Guy":("Greetings", "I am"), "Aldith":("Hello", "I am"), "Rowan":("Good morning","I am"), "Borin":("Hi","I am"), "Piere":("Bonjour", "je suis"), "Dora":("Hola", "me llamo"), "Emperor Chu":("Ni hao","Wo jao"), "not important":("Ah", "my name is"), "Aleixo-Defender of man":("Olá", "meu nome é")}
+
+#sword dictionary Key = name values = minimum damage, maxium damage, damage multiplier
+
+weapons = {"sword":(1, 3, 1), "fists":(0,2,1),"axe":(0, 3, 2), "hammer":(2, 4, 2)}
+
+#boss dictionary Key = name values = description, health
+
+bosses = {"King of bananas":("leader of the monkeys", 5), "sea dude":("king of the sea bed", 10)}
+
+#inventory (starting with fists)
+
+inventory = ["fists"]
+
+fires_lit = 0
+
+#global variables end
+
+#open a file called story.txt and write in it
+
+myfile = open("story.txt", 'w')
+
+#myprint function (substitue print with myprint and put it into the file)
+
+def myprint(text):
+  print(text)
+  print(text, file=myfile, flush = True)
+
+#pick function for creating menus and making them idiots proof
+
+def pick(choices):
+    myprint(Fore.WHITE + Style.BRIGHT)
+
+#while loop to make sure you enter a valid value
+
+    valid = False
+    while valid is False:
+
+        #printing the options to choose from
+
+        for i, option in enumerate(choices):
+            myprint(f"\t{i + 1}) {option}")
+
+        #getting the input
+
+        x = input("select an option?\n")
+
+        #is the input an integer?
+
+        try:
+            x = int(x)
+
+            #is the input in the range of choices
+
+            if x in range(1, len(choices) + 1):
+                valid = True
+            #if the input isn't valid but is an integer print...
+            else:
+                myprint(f"{x} is not a valid option, please try again")
+                x = input()
+
+        #if the input isn't an integer print...
+        
+        except:
+            myprint(f"{x} is not a number, please select the numbers not their answers.")
+            x = input()
+
+    #return what you picked from the choices
+
+    return choices[x - 1]
+
+#function for printing areas
+
+def area(title, descrip):
+  myprint(Style.BRIGHT)
+  myprint(f'{Fore.RED}{title.upper()}')
+  myprint(f' {Fore.YELLOW}{descrip}')
+  x=input()
+
+#function for printing scenes
+
+def scene(name, descrip, options=None):
+  myprint(Style.BRIGHT)
+  myprint(f'{Fore.BLUE}{name}')
+  x=input()
+  myprint(f'{Fore.LIGHTBLUE_EX}{Style.BRIGHT}{descrip}')
+  print(Fore.WHITE + Style.BRIGHT)
+
+  #if you specified options ask the user to pick one.
+
+  if options is not None:
+    return pick(options)
+
+
+#function for printing and "making" bosses
+#gets the values from the bosses dictionary
+
+def boss(bossName):
+  description, health = bosses[bossName]
+  myprint(Style.BRIGHT)
+  myprint(f'{Fore.LIGHTMAGENTA_EX}{bossName.upper()}')
+  myprint(f'{Fore.MAGENTA}{description} - {health}hp')
+  return description, health
+
+
+
+def monkey_forest(name, npc, npc2):
+
+  global fires_lit
+
+  greeting,im = npcs[npc]
+  greeting2,im2 = npcs[npc2]
+  answer= scene(npc + " vanishes and you find yourself before an archway leading to a forest", "Will you enter?",["yes", "no"])
+  if answer == "yes":
+   myprint("you slowly walk into the forest.")
+  elif answer == "no":
+    myprint("You stand still at the edge of the forest thinking of how you arrived here.")
+    x=input()
+    myprint("You turn around to see if there is an exit")
+    x=input()
+    answer = scene("As you walk along the dirt leading away from the forest you notice a monkey engulfed by shadow emerge infront of you.", "How do you react?",["Run back to the forest", "fight the monkey head on!"])
+    if answer == "fight the monkey head on!":
+      myprint("the monkey is faster than usual due to it's shadowed form")
+      x=input()
+      myprint("It swiftly climbs onto your head and bites you in neck!")
+      x=input()
+
+      return False
+    elif answer == "Run back to the forest":
+      myprint("you run into the forest and hide behind a tree hoping the monkey doesn't see you")
+      x=input()
+      myprint("the monkey jumps back into the trees leaving you alone")
+      x=input()
+  area("Monkey forest", "home of monkeys")
+  answer = scene("you see someone leaning against a tree 20m away from you", "will you talk to them?",["yes", "no"])
+  if answer == "yes":
+    myprint(f'{greeting2} {name}, {im2} {npc2}. nice to meet you.')
+    x=input()
+    myprint("You will need this sword, the monkeys in this forest are very aggresive since the land was engulfed.")
+    x=input()
+    myprint("You carry on through the forest")
+    x=input()
+    inventory.append("sword")
+  elif answer == "no":
+    myprint("You ignore the peron and carry on through the forest")
+    
+
+  answer = scene("whilst you are walking through the forest 5 monkeys drop down from the trees", "how can you get past", ["fight the monkeys","carry on running through the forest", "surrender to the monkeys"])
+  sword = "sword" in inventory
+  if answer == "fight the monkeys":
+    if sword is True:
+      myprint("you fight off the monkeys with your sword and carry on through the forest!")
+      
+    elif sword is False:
+      myprint("the monkeys swarm you and with no way of defending yourself you die.")
+      return False
+  elif answer == "carry on running through the forest":
+    myprint("you carry on running but another group of monkeys falls infront of you.")
+    x=input()
+    myprint("You run through them also like the coward you are!")
+    x=input()
+    
+    
+  elif answer == "surrender to the monkeys":
+    myprint("The monkeys tie you to tree and starve you.")
+    return False
+  myprint("after walking through the forest you find an oppening in the trees.")
+  x=input()
+  myprint("you enter the clearing...")
+  x=input()
+
+
+  bossName = "King of bananas"
+  description, health = boss(bossName)
+  for turn in range (3):
+    answer = scene("", "What weapon do you attack with?", inventory)
+    min, max, multiplier = weapons[answer]
+    damage = random.randint(min,max)*multiplier
+    health -= damage
+    if health <= 0:
+      health = 0
+    myprint(f'you did {damage} damage to  {bossName} with {answer}\n{health}hp left')
+    if health <= 0:
+      break
+  if health <=0:
+    x=input()
+    myprint(f'{bossName} fell to ground, he has been defeated')
+    x=input()
+    answer = scene("At the end of the clearing you see a torch with a white flame and an eternal bonfire", "Do you light it?", ["yes", "no"])
+    if answer == "yes":
+      fires_lit += 1
+      myprint("you lit the Monkey forest's flame of light!")
+    elif answer == "no":
+      myprint("you didn't light the fire and the monkey forest remained in shadows")
+    return True
+  elif health >0:
+    x=input()
+    myprint("The king of bananas saw an opening and jumped on you!")
+    x=input()
+    myprint("you were squashed in the process")
+    
+
+
+def story():
+  
+  #sys.stdout = open('story.text', 'w')
+  
+
+
+  npc = random.choice (list(npcs.keys()))
+  npc2 = random.choice (list(npcs.keys()))
+  #npc = list(npcs.keys())[-1]
+  
+  greeting,im = npcs[npc]
+  
+  mimick1 = random.randint(1,3)
+  myprint(Style.BRIGHT)
+  name = input("What is your name?\n")
+  myprint(f'{greeting} {name}, {im} {npc}. Welcome to...')
+  x=input()  
+  area("umbra", "The land of shadows")
+  myprint(Fore.WHITE + "This land is plagued with the curse of the shadows.")
+  x=input()
+  myprint("The shadows can only be destroyed by lighting each regions flame of light.")
+  x=input()
+  myprint("Your journey will be cruel and unforgiving, but I trust you can cure the curse.")
+  x=input()
+  if monkey_forest(name, npc, npc2) is True:
+    x=input()
+    myprint(Fore.LIGHTYELLOW_EX + "You survived the Monkey Forest")
+
+    
+  else: 
+    x=input()
+    area("You died", "Umbra will remain in shawdow")
+
+
+init()
+story()
+myfile.close()
