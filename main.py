fc5ea8039b4644dad399505aab308cfa973237c4
Jack Woolterton <jwoolter@gmail.com>
2020-06-10 05:09:10 +0000

    stuff

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ npcs = {"Guy": ("Greetings", "I am"), "Aldith": ("Hello", "I am"), "Rowan": ("Go
 
 # sword dictionary Key = name values = minimum damage, maxium damage, damage multiplier
 
-weapons = {"sword": (1, 3, 1), "fists": (0, 2, 1), "axe": (0, 3, 2), "spear": (3,4,1), "hammer": (2, 8, 1), "tentacle":(0, 1, 10)}
+weapons = {"sword": (1, 3, 1), "fists": (0, 2, 1), "axe": (0, 6, 1), "spear": (3,4,1), "hammer": (2, 8, 1), "tentacle":(0, 1, 10)}
 
 # boss dictionary Key = name values = description, health
 
-bosses = {"King of bananas": ("leader of the monkeys", 5, ["finds an opening and jumps on you", "you get squashed in the process"]), "Litius": ("pincher of men", 6, ["litius opens his pincer and grabs you","you have been crushed"]), "squid": ("creater of storms", 8, ["waves his tentacle and slaps you", "you fly into the sea unconscious"])}
+bosses = {"King of bananas": ("leader of the monkeys", 5, ["finds an opening and jumps on you", "you get squashed in the process"]), "Litius": ("pincher of men", 6, ["litius opens his pincer and grabs you","you have been crushed"]), "Mr. Squid": ("creater of storms", 8, ["waves his tentacle and slaps you", "you fly into the sea unconscious"])}
 
 # inventory (starting with fists)
 
@@ -314,7 +314,7 @@ def black_sea(name, npc, npc2):
     x=input()
     myprint("and on it...")
     x=input()
-    if boss_fight("squid") is False:
+    if boss_fight("Mr. Squid") is False:
       return False
     myprint("the storm faded with the squid")
     x=input()
